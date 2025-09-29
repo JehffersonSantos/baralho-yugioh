b2b129ff402d93c15238330c842ef3de74fcbdc1
unknown <jefersonsantos10@hotmail.com>
2025-09-29 11:31:03 -0300

    Tentando colocar o efeito de trocar carta

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const selecionarCard = document.querySelectorAll('.nome_card');
 const imagemCard = document.querySelector('.image');
 
+const criarCard = document.createElement('img');
+
 selecionarCard.forEach((btn) => {
     btn.addEventListener('click', (e) => {
 
@@ -9,7 +11,8 @@ selecionarCard.forEach((btn) => {
 
         const id = button.getAttribute('id');
 
-        imagemCard.classList.add("changing");
+        imagemCard.classList.add("trocando");
+        imagemCard.classList.toggle("trocando");
         imagemCard.setAttribute('src', `images/image_${id}.jpg`);
     
     });
