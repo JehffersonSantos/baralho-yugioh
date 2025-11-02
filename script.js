30584b153eafbb238481b5aa5f661bd95f4aa8e5
JehffersonSantos <jefersonsantos10@hotmail.com>
2025-11-02 01:24:52 -0300

    terminando o html/css da pagina

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const selecionarCard = document.querySelectorAll('.nome_card');
-const imagemCard = document.querySelector('.image');
+const imagemCard = document.querySelector('.carta_imagem');
+const botaoDescricao = document.querySelector('.botao_descricao');
+
 const carta_virada = 'images/image_9.jpg';
 
+const descricao = {
+  1: "Um guerreiro esqueleto que surge das trevas.",
+  2: "Um dragão lendário com olhos azuis e poder devastador.",
+  3: "O mago supremo dominando a magia negra.",
+  4: "Um dragão negro que queima tudo em seu caminho.",
+  5: "Um dragão meteórico de chamas intensas.",
+  6: "Carta mágica usada para realizar fusões poderosas.",
+  7: "Um dos três deuses egípcios, domina os céus.",
+  8: "Um dragão amaldiçoado que serve aos guerreiros do caos."
+};
 
 
+
+botaoDescricao.addEventListener('click', (e) => {
+  console.log("botão clicado");
+
+  
+
+
+});
 
 // function trocarCarta(id) {
 
