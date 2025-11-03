e6dd2154f282b383d2a09cd201b27ec021b4946f
JehffersonSantos <jefersonsantos10@hotmail.com>
2025-11-03 10:54:59 -0300

    terminando a base do glossario de yu gi oh

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const selecionarCard = document.querySelectorAll('.nome_card');
 const imagemCard = document.querySelector('.carta_imagem');
 const botaoDescricao = document.querySelector('.botao_descricao');
-const descricaoCarta = document.querySelector('.descricao_carta')
+const descricaoCarta = document.querySelector('.descricao_carta');
+const seta = document.querySelectorAll('.setas');
+const descricao_carta = document.querySelector('.descricao_carta')
 
 const carta_virada = 'images/image_9.jpg';
 
@@ -13,7 +15,8 @@ const descricao = {
   5: "Um dragão meteórico de chamas intensas.",
   6: "Carta mágica usada para realizar fusões poderosas.",
   7: "Um dos três deuses egípcios, domina os céus.",
-  8: "Um dragão amaldiçoado que serve aos guerreiros do caos."
+  8: "Um dragão amaldiçoado que serve aos guerreiros do caos.",
+  9: " Parte de tras das cartas do Yu gi oh"
 };
 
 
@@ -21,12 +24,45 @@ const descricao = {
 botaoDescricao.addEventListener('click', (e) => {
   console.log("botão clicado");
 
-    const ativo = botaoDescricao.classList.toggle('ativo');
+  const ativo = botaoDescricao.classList.toggle('ativo');
+  console.log(ativo)
   
   descricaoCarta.style.display = ativo ? 'block' : 'none';
   console.log(descricaoCarta)
   botaoDescricao.textContent = ativo ? 'Esconder Descrição' : 'Mostrar Descrição';
 
+});
+
+console.log(descricao[1])
+
+
+seta.forEach((seta) => {
+    seta.addEventListener('click', (e) => {
+    
+    const setaEscolhida = e.target.getAttribute('id');
+
+    let separar = imagemCard.getAttribute("src");
+    let cartaAtual = parseInt(separar.split("image_")[1]);
+
+    console.log(setaEscolhida);
+    console.log(separar);
+    console.log(cartaAtual);
+
+
+    if(setaEscolhida === "esquerda"){
+      cartaAtual = cartaAtual > 1 ? cartaAtual - 1 : 8;
+    }else if (setaEscolhida === "direita")
+    {
+      cartaAtual = cartaAtual < 8 ? cartaAtual + 1 : 1;
+    }
+
+    console.log(cartaAtual)
+
+    virarCarta()
+    // imagemCard.setAttribute('src', `images/image_${cartaAtual}.jpg`)
+    atualizarDescricao(cartaAtual)
+    trocarCarta(cartaAtual)
+  });
 });
 
 // function trocarCarta(id) {
@@ -36,10 +72,15 @@ botaoDescricao.addEventListener('click', (e) => {
 //     }, 200);
 // };
 
+function atualizarDescricao (cartaAtual) {
+  document.getElementById('descricao_carta').innerText = descricao[cartaAtual]
+}
+
 function trocarCarta(id) { 
     imagemCard.addEventListener('transitionend', () => {
         imagemCard.setAttribute('src', `images/image_${id}.jpg`);
-    });
+    },  { once: true });
+    atualizarDescricao(id);
 };
 
 
@@ -49,6 +90,7 @@ function virarCarta() {
   setTimeout(() => {
   imagemCard.classList.remove("trocando");
   }, 400);
+  
   // imagemCard.addEventListener('transitionend', () => {
   //     imagemCard.setAttribute('src', carta_virada);
   // });
